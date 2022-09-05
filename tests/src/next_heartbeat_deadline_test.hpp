ed28a771a548f0c589d90b83ceaa06baa2a92cc0
Takayama Fumihiko <tekezo@pqrs.org>
2022-09-06 08:21:48 +0900

    Update tests

--- tests/src/next_heartbeat_deadline_test.hpp
+++ tests/src/next_heartbeat_deadline_test.hpp
@@ -9,6 +9,24 @@ void run_next_heartbeat_deadline_test(void) {
     auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
     auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
 
+    // No next_heartbeat_deadline
+    {
+      auto server = std::make_unique<test_server>(dispatcher,
+                                                  std::nullopt);
+
+      auto client1 = std::make_unique<pqrs::local_datagram::client>(dispatcher,
+                                                                    test_constants::server_socket_file_path,
+                                                                    test_constants::client_socket_file_path,
+                                                                    test_constants::server_buffer_size);
+      client1->set_server_check_interval(std::chrono::milliseconds(500));
+      client1->async_start();
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+      auto counts = server->get_next_heartbeat_deadline_exceeded_counts();
+      expect(counts.size() == 0_i);
+    }
+
     // Enough next_heartbeat_deadline
     {
       auto server = std::make_unique<test_server>(dispatcher,
